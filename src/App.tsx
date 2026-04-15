9c5ba55c8cfe0f6379e083694bd611fc7741df9b
Kelly Cason <kellycason@microsoft.com>
2026-04-15 11:19:41 -0500

    Update router with all 6 routes: home, license, registration, appointments, documents, faq

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
-import About from './pages/About'
+import LicenseRenewal from './pages/LicenseRenewal'
+import VehicleRegistration from './pages/VehicleRegistration'
+import Appointments from './pages/Appointments'
+import Documents from './pages/Documents'
+import FAQ from './pages/FAQ'
 
 export default function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/license-renewal" element={<LicenseRenewal />} />
+        <Route path="/vehicle-registration" element={<VehicleRegistration />} />
+        <Route path="/appointments" element={<Appointments />} />
+        <Route path="/documents" element={<Documents />} />
+        <Route path="/faq" element={<FAQ />} />
       </Routes>
     </Layout>
   )
 }
+
