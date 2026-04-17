4722ccc9b4cdcd24afb865cf2e716b75464f698c
Kelly Cason <kellycason@microsoft.com>
2026-04-17 13:22:48 -0500

    Sync sitemap icons from Power Platform: add SVG icons for citizen, dealer, term, payment; pull all icon web resources

--- dataverse/12_fix_sitemap_png.ps1
+++ dataverse/12_fix_sitemap_png.ps1
@@ -11,27 +11,32 @@ $smId = "98db46b0-896f-412f-a212-01d534198efb"
 
 $sitemapXml = '<SiteMap IntroducedVersion="7.0.0.0">' +
   '<Area Id="DMVArea" Title="DMV Operations" ShowGroups="true">' +
+    '<Group Id="DashboardGroup" Title="Dashboards">' +
+      '<SubArea Id="nav_regdashboard" Url="$webresource:dmv_/dashboard/registration_dashboard.html" Title="Registration Operations" Icon="$webresource:dmv_/icons/dashboard_icon.png" AvailableOffline="true" PassParams="false" />' +
+    '</Group>' +
     '<Group Id="CitizenGroup" Title="Citizen Services">' +
-      '<SubArea Id="nav_contact" Entity="contact" Title="Citizens (Contacts)" Icon="$webresource:dmv_/icons/citizen_icon.png" />' +
-      '<SubArea Id="nav_driverlicense" Entity="dmv_driverlicense" Title="Driver Licenses" Icon="$webresource:dmv_/icons/license_icon.png" />' +
-      '<SubArea Id="nav_appointment" Entity="dmv_appointment" Title="Appointments" Icon="$webresource:dmv_/icons/appointment_icon.png" />' +
-      '<SubArea Id="nav_documentupload" Entity="dmv_documentupload" Title="Document Uploads" Icon="$webresource:dmv_/icons/document_icon.png" />' +
-      '<SubArea Id="nav_notification" Entity="dmv_notification" Title="Notifications" Icon="$webresource:dmv_/icons/notification_icon.png" />' +
+      '<SubArea Id="nav_contact" Entity="contact" Title="Citizens (Contacts)" VectorIcon="/WebResources/dmv_/icons/citizen.svg" Icon="/WebResources/dmv_/icons/citizen.svg" AvailableOffline="true" PassParams="false" />' +
+      '<SubArea Id="nav_driverlicense" Entity="dmv_driverlicense" Title="Driver Licenses" Icon="$webresource:dmv_/icons/license_icon.png" AvailableOffline="true" PassParams="false" />' +
+      '<SubArea Id="nav_appointment" Entity="dmv_appointment" Title="Appointments" Icon="$webresource:dmv_/icons/appointment_icon.png" AvailableOffline="true" PassParams="false" />' +
+      '<SubArea Id="nav_documentupload" Entity="dmv_documentupload" Title="Document Uploads" Icon="$webresource:dmv_/icons/document_icon.png" AvailableOffline="true" PassParams="false" />' +
+      '<SubArea Id="nav_notification" Entity="dmv_notification" Title="Notifications" Icon="$webresource:dmv_/icons/notification_icon.png" AvailableOffline="true" PassParams="false" />' +
     '</Group>' +
     '<Group Id="VehicleGroup" Title="Vehicle Services">' +
-      '<SubArea Id="nav_vehicle" Entity="dmv_vehicle" Title="Vehicles" Icon="$webresource:dmv_/icons/vehicle_icon.png" />' +
-      '<SubArea Id="nav_vehiclereg" Entity="dmv_vehicleregistration" Title="Registrations" Icon="$webresource:dmv_/icons/registration_icon.png" />' +
-      '<SubArea Id="nav_vehicletitle" Entity="dmv_vehicletitle" Title="Vehicle Titles" Icon="$webresource:dmv_/icons/title_icon.png" />' +
-      '<SubArea Id="nav_lien" Entity="dmv_lien" Title="Liens" Icon="$webresource:dmv_/icons/lien_icon.png" />' +
+      '<SubArea Id="nav_vehicle" Entity="dmv_vehicle" Title="Vehicles" Icon="$webresource:dmv_/icons/vehicle_icon.png" AvailableOffline="true" PassParams="false" />' +
+      '<SubArea Id="nav_vehiclereg" Entity="dmv_vehicleregistration" Title="Registrations" Icon="$webresource:dmv_/icons/registration_icon.png" AvailableOffline="true" PassParams="false" />' +
+      '<SubArea Id="nav_regterm" Entity="dmv_registrationterm" Title="Registration Terms" VectorIcon="/WebResources/dmv_/icons/term.svg" Icon="/WebResources/dmv_/icons/term.svg" AvailableOffline="true" PassParams="false" />' +
+      '<SubArea Id="nav_regpayment" Entity="dmv_registrationpayment" Title="Registration Payments" VectorIcon="/WebResources/dmv_/icons/payment.svg" Icon="/WebResources/dmv_/icons/payment.svg" AvailableOffline="true" PassParams="false" />' +
+      '<SubArea Id="nav_vehicletitle" Entity="dmv_vehicletitle" Title="Vehicle Titles" Icon="$webresource:dmv_/icons/title_icon.png" AvailableOffline="true" PassParams="false" />' +
+      '<SubArea Id="nav_lien" Entity="dmv_lien" Title="Liens" Icon="$webresource:dmv_/icons/lien_icon.png" AvailableOffline="true" PassParams="false" />' +
     '</Group>' +
     '<Group Id="DealerGroup" Title="Dealer Operations">' +
-      '<SubArea Id="nav_account" Entity="account" Title="Dealers (Accounts)" Icon="$webresource:dmv_/icons/dealer_icon.png" />' +
-      '<SubArea Id="nav_temporarytag" Entity="dmv_temporarytag" Title="Temporary Tags" Icon="$webresource:dmv_/icons/temptag_icon.png" />' +
-      '<SubArea Id="nav_bulksubmission" Entity="dmv_bulksubmission" Title="Bulk Submissions" Icon="$webresource:dmv_/icons/bulk_icon.png" />' +
+      '<SubArea Id="nav_account" Entity="account" Title="Dealers (Accounts)" VectorIcon="/WebResources/dmv_/icons/dealer.svg" Icon="/WebResources/dmv_/icons/dealer.svg" AvailableOffline="true" PassParams="false" />' +
+      '<SubArea Id="nav_temporarytag" Entity="dmv_temporarytag" Title="Temporary Tags" Icon="$webresource:dmv_/icons/temptag_icon.png" AvailableOffline="true" PassParams="false" />' +
+      '<SubArea Id="nav_bulksubmission" Entity="dmv_bulksubmission" Title="Bulk Submissions" Icon="$webresource:dmv_/icons/bulk_icon.png" AvailableOffline="true" PassParams="false" />' +
     '</Group>' +
     '<Group Id="AdminGroup" Title="Administration">' +
-      '<SubArea Id="nav_dmvoffice" Entity="dmv_dmvoffice" Title="DMV Offices" Icon="$webresource:dmv_/icons/office_icon.png" />' +
-      '<SubArea Id="nav_transactionlog" Entity="dmv_transactionlog" Title="Transaction Log" Icon="$webresource:dmv_/icons/transaction_icon.png" />' +
+      '<SubArea Id="nav_dmvoffice" Entity="dmv_dmvoffice" Title="DMV Offices" Icon="$webresource:dmv_/icons/office_icon.png" AvailableOffline="true" PassParams="false" />' +
+      '<SubArea Id="nav_transactionlog" Entity="dmv_transactionlog" Title="Transaction Log" Icon="$webresource:dmv_/icons/transaction_icon.png" AvailableOffline="true" PassParams="false" />' +
     '</Group>' +
   '</Area>' +
 '</SiteMap>'
